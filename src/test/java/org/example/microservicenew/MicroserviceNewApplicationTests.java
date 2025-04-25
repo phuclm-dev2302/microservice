4b2e24770f9200e6042ff74fec221d1d141e05d4
Phuc <lephuc04b3@gmail.com>
2025-04-25 12:56:01 +0700

    first commit

---
+++ src/test/java/org/example/microservicenew/MicroserviceNewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.microservicenew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
