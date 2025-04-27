9d7585eceb6857a6fedf0c4ef2a2307e3daf2906
Phuc <lephuc04b3@gmail.com>
2025-04-27 20:29:20 +0700

    feat: add InventoryRepository

---
+++ inventory-service/src/main/java/com/example/inventory_service/repository/InventoryRepository.java
@@ -0,0 +1,12 @@
+package com.example.inventory_service.repository;
+
+import com.example.inventory_service.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface InventoryRepository extends JpaRepository<Inventory,String> {
+    Optional<Inventory> findBySkuCode(String skuCode);
+}
