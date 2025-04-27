e366351e163bd32c9220d27b2f9380d2f9637465
Phuc <lephuc04b3@gmail.com>
2025-04-27 20:29:12 +0700

    feat: add InventoryController

---
+++ inventory-service/src/main/java/com/example/inventory_service/controller/InventoryController.java
@@ -0,0 +1,20 @@
+package com.example.inventory_service.controller;
+
+import com.example.inventory_service.service.InventoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("api/v1/inventory")
+@Repository
+public class InventoryController {
+    @Autowired
+    private InventoryService inventoryService;
+
+    @GetMapping("/{sku-code}")
+    public boolean isInStock(@PathVariable("sku-code") String skuCode) {
+        return inventoryService.isInStock(skuCode);
+    }
+}
