9cf320a6699f0a7ca3e175877bdb0f91307c8bf2
Phuc <lephuc04b3@gmail.com>
2025-04-27 20:31:05 +0700

    feat: add OrderServiceApplication

--- order-service/src/main/java/com/example/order_service/OrderServiceApplication.java
+++ order-service/src/main/java/com/example/order_service/OrderServiceApplication.java
@@ -1,13 +1,13 @@
-package org.example.microservicenew;
+package com.example.order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MicroserviceNewApplication {
+public class OrderServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceNewApplication.class, args);
+		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 
 }
--- order-service/src/test/java/com/example/order_service/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/example/order_service/OrderServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package org.example.microservicenew;
+package com.example.order_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MicroserviceNewApplicationTests {
+class OrderServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
