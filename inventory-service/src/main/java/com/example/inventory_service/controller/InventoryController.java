ff60f0f6d8294631335e42b3f829fba2b9e95dd0
Phuc <lephuc04b3@gmail.com>
2025-04-29 15:05:51 +0700

    refactor: customise function isInStock

--- inventory-service/src/main/java/com/example/inventory_service/controller/InventoryController.java
+++ inventory-service/src/main/java/com/example/inventory_service/controller/InventoryController.java
@@ -1,20 +1,22 @@
 package com.example.inventory_service.controller;
 
+import com.example.inventory_service.dto.InventoryResponse;
 import com.example.inventory_service.service.InventoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("api/v1/inventory")
-@Repository
+@RestController
 public class InventoryController {
     @Autowired
     private InventoryService inventoryService;
 
-    @GetMapping("/{sku-code}")
-    public boolean isInStock(@PathVariable("sku-code") String skuCode) {
+    //      http://localhost:8083/api/v1/inventory?sku-code=iphone-13&sku-code=iphone-14
+    @GetMapping()
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
         return inventoryService.isInStock(skuCode);
     }
 }
